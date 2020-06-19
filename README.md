2f33f34363e8ddd4b15893cc6da36d2a26cec76e
Roger Takeshita <roger.takeshita@gmail.com>
2020-06-19 12:45:21 -0400

    Update touch-open.sh
        - Refactored the script, now it's possible to create nested folder/files
        - More details on README

--- README.md
+++ README.md
@@ -68,17 +68,36 @@
 
 -   [File Link](https://github.com/Roger-Takeshita/Shell-Script/blob/master/touch-open.sh)
 -   Search if path and file already exist, otherwise, it will create the folder(s) and the file
-    -   if you pass `-n` as the first argument, the script wont open the files after creating them
+
+    -   if we pass `-n` as the first argument, the script wont open the files after creating them
         -   `touch -n test/test.js test1/test1.js`
         -   My default editor is the VSCode
-    -   if you pass `+` between files, it will create the next file inside the previous folder (for now it's only working for 1 level of folder)
-        -   `touch test1.js test/test1.js + test2.js`
+    -   if we pass `+` between files, it will create the next file inside the previous folder, we can nest multiple folder/files
+        -   unfortunately we can't go back to the previous folder, we can only create the file on the same level or above
+    -   if we pass a path without an extension, the program understands that we want to create a folder
+
+        -   `touch folder/subfolder`
+        -   `touch outside1.js head1/node1/tail1/leaf1.js + leaf2.js head2/node2/node2a/tail2a/leaf2a1.js + leaf2a2.js head2/node2/node2b/tail2b/leaf2b1.js + leaf2b2.js`
+
             ```Bash
-              .
-              ├── test1.js
-              └── test
-                  ├── test1.js
-                  └── test2.js
+              ├─ head1
+              │  └─ node1
+              │     └─ tail1
+              │        ├─ leaf1.js
+              │        └─ leaf2.js
+              ├─ head2
+              │  └─ node2
+              │     ├─ node2a
+              │     │  └─ tail2a
+              │     │     ├─ leaf2a1.js
+              │     │     └─ leaf2a2.js
+              │     └─ node2b
+              │        └─ tail2b
+              │           ├─ leaf2b1.js
+              │           └─ leaf2b2.js
+              ├─ outside1.js
+              ├─ folder
+              │  └─ subfolder
             ```
 
 <h1 id='bashshell'>Bash Shell</h1>
--- touch-open.sh
+++ touch-open.sh
@@ -1,82 +1,80 @@
-#!/bin/zsh
-
-dontOpen=0
-sameDir=0;
-previousDir=""
+#!/bin/bash
 
 if [ $# -eq 0 ]; then
     echo "\e[0;31mERROR:\e[37m I need a file" >&2
     return 1
 fi
 
-for fileName do
-    create=1
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
 
-    if [[ $fileName =~ (^-.+$) ]]; then
+previousDir=""
+shouldOpenFile=1;
+usePreviousDir=0
+
+for path in $@
+do
+    IFS='/' read -ra items <<< "$path"
+    dir=''
+
+    if [[ $path =~ (^-.+$) ]]; then
         # echo " ${fileName}"
 
-        if [ "$fileName" = "-n" ]; then
-            dontOpen=1
+        if [ "$path" = "-n" ]; then
+            shouldOpenFile=0;
         fi
-        
+
         continue
     fi
 
-    if [ $fileName = "+" ]; then
-        if [ "${previousDir}" = "" ]; then
-            sameDir=0
-        else
-            sameDir=1
-            dir=$previousDir
+    if [ $path = "+" ]; then
+        usePreviousDir=1
+    elif [ $usePreviousDir -eq 0 ]; then
+        dir=""
+        previousDir=""
+    fi 
+
+    for item in ${items[@]}
+    do
+        if [ $item = "+" ]; then
+            continue
         fi
 
-        continue
-    else
-        if [ $sameDir -eq 1 ]; then
-            dir="${previousDir}/${fileName}"
-        else
-            dir=$(dirname "${fileName}")
+        if [[ ${item%.*} == ${item##*.} ]]; then
+            if [ $usePreviousDir -eq 1 ]; then
+                dir="${previousDir}${item}/"
+            else
+                dir="${dir}${item}/"
+            fi
+
             previousDir=$dir
+
+
+            if [ ! -d "$dir" ]; then
+                mkdir -p "$dir"
+            fi
+        else 
+            file=$previousDir$item
+            usePreviousDir=0
+
+            if [ ! -f $file ]; then
+                touch ${file}
+
+                if [ $shouldOpenFile -eq 1 ]; then
+                    code "$file"
+                fi
+            else
+                printf "%s\n" "${previousDir}${grn}${item}${end} - ${red}Already Exists${end}"
+
+                if [ $shouldOpenFile -eq 1 ]; then
+                    code "$file"
+                fi
+            fi 
         fi
-    fi
-
-    if [ -f "$fileName" ]; then
-        echo "\e[37m$fileName \e[0;31malready exists"
-
-        if [ $dontOpen -eq 0 ]; then
-            code "$fileName"
-        fi
-
-        create=0
-        continue
-    elif [ -f "${previousDir}/${fileName}" ]; then
-        echo "\e[37m${previousDir}/${fileName} \e[0;31malready exists"
-
-        if [ $dontOpen -eq 0 ]; then
-            code "${previousDir}/${fileName}"
-        fi
-
-        create=0
-        continue
-    fi
-
-    if [ ! -d "$dir" ]; then
-        if [ $sameDir -eq 1 ]; then
-            sameDir=0
-            create=0
-        else
-            mkdir -p "$dir"
-            create=1
-        fi
-    fi
-
-    if [ $dontOpen -eq 0 ] && [ $create -eq 1 ]; then
-        touch "$fileName" && code "$fileName"
-    elif [ $dontOpen -eq 1 ] && [ $create -eq 1 ]; then
-        touch "$fileName"
-    elif [ $dontOpen -eq 0 ] && [ $create -eq 0 ]; then
-        touch "${previousDir}/${fileName}" && code "${previousDir}/${fileName}"
-    elif [ $dontOpen -eq 1 ] && [ $create -eq 0 ]; then
-        touch "${previousDir}/${fileName}"
-    fi
+    done
 done
