2beffb4bc5ac0a81864000788ca3f8d643014f3f
Roger Takeshita <roger.takeshita@gmail.com>
2020-09-04 16:54:17 -0400

    git-status update the colors and properties to display for a single folder or multiple folders

--- git-status.sh
+++ git-status.sh
@@ -7,47 +7,69 @@ tput reset
 
 dir="`pwd`"
 red=$'\e[0;31m'
-grn=$'\e[0;32m'
 yel=$'\e[0;33m'
-blu=$'\e[0;34m'
-mag=$'\e[0;35m'
-cyn=$'\e[0;36m'
 gry=$'\e[0;2m'
 end=$'\e[0m'
+mode='single-folder'
 
-gitCheck() {
-	if [ -d "$1/.git" ]; then
-		mod=0
-		cd $1
-
-		if [ $(git status | grep modified -c) -ne 0 ]; then
-			mod=1
-			echo "${yel}Modified File(s)   --> ${1##*/}${end}"
-		fi
-
-		if [ $(git status | grep Untracked -c) -ne 0 ]; then
-			mod=1
-			echo "${red}Untracked File(s)  --> ${1##*/}${end}"
-		fi
-
-		if [ $(git status | grep 'Your branch is ahead' -c) -ne 0 ]; then
-			mod=1
-			echo "${yel}Unpushed Commit(s) --> ${1##*/}${end}"
-		fi
-
-		if [ $mod -eq 0 ]; then
-			echo "${gry}Nothing to Commit  --> ${1##*/}${end}"
-		fi
-
-		cd ../
-	fi
+echoFolderName() {
+    if [ "$mode" = "single-folder" ]; then
+        echo "${red}Modified/Untracked"
+    else if
+        echo "${red}Modified/Untracked --> ${1##*/}${end}"
+    fi
 }
 
-if [ -d "$dir/.git" ]; then
-	gitCheck $dir/
-else
-	for f in $dir/*
-	do
-		gitCheck $f
-	done
-fi
+gitStatus() {
+    if [ -d "$1/.git" ]; then
+        statusFlag=0
+        cd $1
+
+        if [ $(git status | grep modified -c) -ne 0 ]; then
+            if [ $statusFlag -eq 0 ]; then
+                echoFolderName $1
+                statusFlag=1
+            fi
+        fi
+
+        if [ $(git status | grep Untracked -c) -ne 0 ]; then
+            if [ $statusFlag -eq 0 ]; then
+                echoFolderName $1
+                statusFlag=1
+            fi
+        fi
+
+        unpushedCommit=$(git status | grep 'Your branch is ahead' -c)
+        if [ $unpushedCommit -ne 0 ]; then
+            if [ $statusFlag -eq 0 ]; then
+                echoFolderName $1
+                statusFlag=1
+            fi
+
+            if [ $unpushedCommit -eq 1 ]; then
+                echo "${yel}  $unpushedCommit Unpushed Commit"
+            else
+                echo "${yel}  $unpushedCommit Unpushed Commits"
+            fi
+        fi
+
+        if [ $statusFlag -eq 0 ]; then
+            if [ "$mode" = "single-folder" ]; then
+                echo "${gry}Nothing to Commit"
+            else if
+                echo "${gry}Nothing to Commit  --> ${1##*/}${end}"
+            fi
+        fi
+
+        git status -s
+        cd ../
+    fi
+}
+
+gitStatus $dir/
+
+for f in $dir/*
+do
+    mode='nested-folder'
+    gitStatus $f
+done
