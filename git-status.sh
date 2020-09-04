e2cef22b593095da40ce4e747a2edbea61c03689
Roger Takeshita <roger.takeshita@gmail.com>
2020-09-04 03:15:45 -0400

    Updated the colors and dried the code

--- git-status.sh
+++ git-status.sh
@@ -1,58 +1,49 @@
 #!/bin/zsh
+
+# Developed by Roger Takeshita
+# https://github.com/Roger-Takeshita/Shell-Script
+
 tput reset
 
-dir="$1"
+dir="`pwd`"
+red=$'\e[0;31m'
+grn=$'\e[0;32m'
+yel=$'\e[0;33m'
+blu=$'\e[0;34m'
+mag=$'\e[0;35m'
+cyn=$'\e[0;36m'
+gry=$'\e[0;2m'
+end=$'\e[0m'
 
-if [ -z "$dir" ]
-then
-    dir="`pwd`"
-fi
-
-gitCheck(){
-	if [ -d "$1/.git" ]
-	then
+gitCheck() {
+	if [ -d "$1/.git" ]; then
 		mod=0
 		cd $1
 
-		if [ $(git status | grep modified -c) -ne 0 ]
-		then
+		if [ $(git status | grep modified -c) -ne 0 ]; then
 			mod=1
-			echo -en "\033[0;33m"
-			echo "Modified File(s)   --> \033[33m${1##*/}"
-			echo -en "\e[2m"
+			echo "${yel}Modified File(s)   --> ${1##*/}${end}"
 		fi
 
-		if [ $(git status | grep Untracked -c) -ne 0 ]
-		then
+		if [ $(git status | grep Untracked -c) -ne 0 ]; then
 			mod=1
-			echo -en "\e[0;31m"
-			echo "Untracked File(s)  --> \e[0;31m${1##*/}"
+			echo "${red}Untracked File(s)  --> ${1##*/}${end}"
 		fi
 
-		if [ $(git status | grep 'Your branch is ahead' -c) -ne 0 ]
-		then
+		if [ $(git status | grep 'Your branch is ahead' -c) -ne 0 ]; then
 			mod=1
-			echo -en "\033[0;32m"
-			echo "Unpushed Commit(s) --> \033[0;32m${1##*/}"
+			echo "${yel}Unpushed Commit(s) --> ${1##*/}${end}"
 		fi
 
-		if [ $mod -eq 0 ]
-		then
-			echo -en "\e[37m"
-			echo -en "\e[2m"
-			echo "Nothing to Commit  --> ${1##*/}"
+		if [ $mod -eq 0 ]; then
+			echo "${gry}Nothing to Commit  --> ${1##*/}${end}"
 		fi
 
 		cd ../
-	# else
-	# 	echo "Not a git repository"
 	fi
-
-	# echo
 }
 
-if [ -d "$dir/.git" ]
-then
+if [ -d "$dir/.git" ]; then
 	gitCheck $dir/
 else
 	for f in $dir/*
