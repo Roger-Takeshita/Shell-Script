625d28944588732fde4a2747043c57e9d35e0027
Roger Takeshita <roger.takeshita@gmail.com>
2020-09-07 00:00:53 -0400

    update clean-my-node to delete node_modules and package-log.json inside all nested folder, it will stop on the first match

--- clean-my-node.sh
+++ clean-my-node.sh
@@ -3,69 +3,39 @@
 # Developed by Roger Takeshita
 # https://github.com/Roger-Takeshita/Shell-Script
 
-tput reset
+red=$'\e[0;31m'
+green=$'\e[0;32m'
+orange=$'\e[38;5;202m'
+end=$'\e[0m'
+
+dim=$'\e[2m'
+bold=$'\e[1m'
 
 dir="`pwd`"
-red=$'\e[0;31m'
-end=$'\e[0m'
-grn=$'\e[0;32m'
-fade=$'\e[2m'   # fade
-bold=$'\e[1m'   # bold fade
-statusFlag=0
 
-freeCurrentFolder() {
-    if [ -d "node_modules" ]; then
-        rm -rf "node_modules"
-        echo "${red}${bold}Deleted${end} node_modules"
-        statusFlag=1
-    fi
-    if [ -f "package-lock.json" ]; then
-        rm "package-lock.json"
-        echo "${red}${bold}Deleted${end} package-lock.json"
-        statusFlag=1
-    fi
+checkFolder() {
+    for fileFolder in "$1"/*; do
+        delete=0
+
+        if [ -d $fileFolder ] && [ -d "$fileFolder/node_modules" ]; then
+            rm -rf "$fileFolder/node_modules"
+            echo "${red}${bold}Deleted${end} $fileFolder/${orange}node_modules"${end}
+            delete=1
+        fi
+        if [ -d $fileFolder ] && [ -f "$fileFolder/package-lock.json" ]; then
+            rm "$fileFolder/package-lock.json"
+            echo "${red}${bold}Deleted${end} $fileFolder/${orange}package-lock.json"${end}
+            delete=1
+        fi
+        if [ $delete -eq 1 ]; then
+            continue
+        fi
+        
+        if [ -d $fileFolder ]; then
+            checkFolder $fileFolder
+        fi
+    done
 }
 
-freeNestedFolder() {
-    if [ -d $f ]; then
-        echo "${red}$f"
-
-        if [ -d "$f/node_modules" ]; then
-            rm -rf "$f/node_modules"
-            echo "${red}${bold}Deleted${end} $f/node_modules"
-            statusFlag=1
-        fi
-        if [ -f "$f/package-lock.json" ]; then
-            rm "$f/package-lock.json"
-            echo "${red}${bold}Deleted${end} $f/package-lock.json"
-            statusFlag=1
-        fi
-
-        for f in $1/*
-        do
-            if [ -d "$f/node_modules" ]; then
-                rm -rf "$f/node_modules"
-                echo "${red}${bold}Deleted${end} $f/node_modules"
-                statusFlag=1
-            fi
-            if [ -f "$f/package-lock.json" ]; then
-                rm "$f/package-lock.json"
-                echo "${red}${bold}Deleted${end} $f/package-lock.json"
-                statusFlag=1
-            fi
-        done
-    fi
-}
-
-freeCurrentFolder
-
-for f in $dir/*
-do
-    freeNestedFolder $f
-done
-
-if [ $statusFlag -eq 1 ]; then
-    echo "${grn}Done"
-else
-    echo "${grn}All Good"
-fi
+checkFolder $dir
+echo ${green}All Good
