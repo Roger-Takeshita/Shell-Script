17dc9cf14531b59ae99f79ae13c67aafe9cfed18
Roger Takeshita <roger.takeshita@gmail.com>
2020-09-27 11:17:10 -0400

    Add Package-lock.json back, the only node_modules will be deleted

--- clean-my-node.sh
+++ clean-my-node.sh
@@ -20,13 +20,13 @@ checkFolder() {
             rm -rf "$fileFolder/node_modules"
             echo "${red}${bold}Deleted${end} $fileFolder/${orange}node_modules"${end}
         fi
-        if [ -f "$baseFolder/package-lock.json" ]; then
-            rm "$baseFolder/package-lock.json"
-            echo "${red}${bold}Deleted${end} $baseFolder/${orange}package-lock.json"${end}
-        elif [ -f "$fileFolder/package-lock.json" ]; then
-            rm "$fileFolder/package-lock.json"
-            echo "${red}${bold}Deleted${end} $fileFolder/${orange}package-lock.json"${end}
-        fi
+        # if [ -f "$baseFolder/package-lock.json" ]; then
+        #     rm "$baseFolder/package-lock.json"
+        #     echo "${red}${bold}Deleted${end} $baseFolder/${orange}package-lock.json"${end}
+        # elif [ -f "$fileFolder/package-lock.json" ]; then
+        #     rm "$fileFolder/package-lock.json"
+        #     echo "${red}${bold}Deleted${end} $fileFolder/${orange}package-lock.json"${end}
+        # fi
         if [ -d "$fileFolder" ] && [ "$(ls $fileFolder)" ]; then
             checkFolder $fileFolder
         fi
