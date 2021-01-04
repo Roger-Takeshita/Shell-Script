f63290dc4a0e814c94978fb606c0f9d907813abf
Roger Takeshita <roger.takeshita@gmail.com>
2021-01-03 23:05:22 -0500

    Update clean-my-node, add node_modules.nosync

--- Scripts/clean-my-node/clean-my-node.sh
+++ Scripts/clean-my-node/clean-my-node.sh
@@ -19,14 +19,13 @@ checkFolder() {
         elif [ -d "$fileFolder/node_modules" ]; then
             rm -rf "$fileFolder/node_modules"
             echo "${red}${bold}Deleted${end} $fileFolder/${orange}node_modules"${end}
+        elif [ -d "$baseFolder/node_modules.nosync" ]; then
+            rm -rf "$baseFolder/node_modules.nosync"
+            echo "${red}${bold}Deleted${end} $baseFolder/${orange}node_modules.nosync"${end}
+        elif [ -d "$fileFolder/node_modules.nosync" ]; then
+            rm -rf "$fileFolder/node_modules.nosync"
+            echo "${red}${bold}Deleted${end} $fileFolder/${orange}node_modules.nosync"${end}
         fi
-        # if [ -f "$baseFolder/package-lock.json" ]; then
-        #     rm "$baseFolder/package-lock.json"
-        #     echo "${red}${bold}Deleted${end} $baseFolder/${orange}package-lock.json"${end}
-        # elif [ -f "$fileFolder/package-lock.json" ]; then
-        #     rm "$fileFolder/package-lock.json"
-        #     echo "${red}${bold}Deleted${end} $fileFolder/${orange}package-lock.json"${end}
-        # fi
         if [ -d "$fileFolder" ] && [ "$(ls $fileFolder)" ]; then
             checkFolder $fileFolder
         fi
