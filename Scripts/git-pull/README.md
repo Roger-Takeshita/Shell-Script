8f203f0bddea6caa0352f8cfecaf7a2e332fe86e
Roger Takeshita <roger.takeshita@gmail.com>
2021-01-15 19:50:03 -0500

    feat: Should add git-pull Scripts

---
+++ Scripts/git-pull/README.md
@@ -0,0 +1,14 @@
+# GIT-PULL
+
+## How to Use It?
+
+- Download the script
+- Create an alias (bash/zshell)
+
+  ```Bash
+    alias git-pull='/Users/roger-that/Documents/Codes/Shell_Script/Scripts/git-pull/git-pull.sh'
+  ```
+
+- Run the script inside a folder that has multiple repositories
+
+  ![](https://i.imgur.com/sfAsX5V.png)
---
+++ Scripts/git-pull/git-pull.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+# Developed by Roger Takeshita
+# https://github.com/Roger-Takeshita/Shell-Script
+
+dir=`pwd`
+RST=$'\e[39m'      # reset color
+RD=$'\e[31m'       # red
+GN=$'\e[32m'       # green
+BL=$'\e[34m'       # blue
+OG=$'\e[38;5;202m' # orange
+Bold=$'\e[1m'
+End=$'\e[0m'       # reset format
+Blink=$'\e[5m'
+NoBlink=$'\e[25m'
+
+
+gitPull() {
+    if [ -d "$1/.git" ]; then
+        main=`cd $1; git show-ref refs/heads/main`
+        master=`cd $1; git show-ref refs/heads/master`
+        branch=''
+
+        echo "$Bold$BL→ $(basename $1)$RST$End"
+
+        if [ -n "$main" ]; then
+            branch="main"
+        elif [ -n "$master" ]; then
+            branch="master"
+        fi
+
+        if [ -n $branch ]; then
+            response=`cd $1; git pull origin $branch`
+
+            if [ "$response" != "Already up to date." ]; then
+                echo "${Blink}${RD}${Bold}Aborted!${End}${RST}${NoBlink} ${OG}Uncommited changes, stash them before you merge.${RST}"
+            else
+                echo "${GN}${response}${RST}"
+            fi
+        fi
+    fi
+}
+
+gitPull $dir/
+
+for f in $dir/*
+do
+    gitPull $f
+done
