7844bd2067c6c2f3a26020965028d0f1e513de8b
Roger Takeshita <roger.takeshita@gmail.com>
2021-02-10 12:39:39 -0500

    feat: Should add some package.json edge cases
        - Added a condition to check if the project has already been tagged
        with the current json version
        - Added more styling to print the correct information for each case
        - Checks if package.json exists before running the script

--- Scripts/git-tag/git-tag.sh
+++ Scripts/git-tag/git-tag.sh
@@ -1,32 +1,60 @@
 #!/bin/bash
 
-RSTC=$'\e[39m'       # reset color
-CGY=$'\e[38;5;240m'  # gray
-CLBL=$'\e[38;5;117m' # light blue
-CLGN=$'\e[38;5;2m'   # light green
-CLOG=$'\e[38;5;215m' # light orange
-CLRD=$'\e[38;5;1m'   # light red
-CLYL=$'\e[38;5;228m' # light yellow
+# Developed by Roger Takeshita
+# https://github.com/Roger-Takeshita/Shell-Script
+
+RSTC=$'\e[39m'        # reset color
+CGY=$'\e[38;5;240m'   # gray
+CWHT=$'\e[38;5;15m'   # white
+CLBL=$'\e[38;5;117m'  # light blue
+CLGN=$'\e[38;5;2m'    # light green
+CLOG=$'\e[38;5;215m'  # light orange
+CLRD=$'\e[38;5;1m'    # light red
+BGRSTC=$'\e[49m'      # bg reset color
+BGCGN=$'\e[48;5;34m'  # bg green
+BGCBL=$'\e[48;5;27m'  # bg blue
+
 DIR=$(pwd)
+FILENAME="package.json"
 
-PACKAGE_VERSION=$(grep -m1 version ${DIR}/package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+if [ -f "${DIR}/${FILENAME}" ]; then
+    PACKAGE_VERSION=$(grep -m1 version ${DIR}/${FILENAME} | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+else
+    echo ""
+    echo "    ${CLBL}${FILENAME}${CLOG} not found.${RSTC}"
+    echo "    ${CLOG}Looks like you are trying to run the script outside of your project root path.${RSTC}"
+    echo "    ${CLRD}Process aborted!"
+    echo ""
+    exit
+fi
 
+# _Maybe we are going to use later
 function splitVersion() {
     if [[ $1 =~ ([0-9]+).([0-9]+).([0-9]+) ]]; then
       MAJOR=${BASH_REMATCH[1]}
       MINOR=${BASH_REMATCH[2]}
-      TINY=${BASH_REMATCH[3]}
+      PATCH=${BASH_REMATCH[3]}
     fi
 }
 
-function checkTagExists() {
-    local TAG=$1
+function trim() {
+    local VAR="$*"
+    VAR="${VAR#"${VAR%%[![:space:]]*}"}"
+    VAR="${VAR%"${VAR##*[![:space:]]}"}"
+    printf '%s' "$VAR"
+}
 
-    if git rev-parse $TAG >/dev/null 2>&1 ; then
-        echo "    ${CLRD}Tag ${CLGN}${TAG}${CLRD} already exists.${RSTC}"
-        TAGS=$(git tag | sed -e 's/\n/ /g' | tr '\r\n' ' ')
-        echo "    ${CGY}Existing tags: ${TAGS}${RSTC}"
+function checkTagExists() {
+    local TAG=$(trim $1)
+
+    if [ "$TAG" != "" ] && git rev-parse $TAG >/dev/null 2>&1 ; then
         echo ""
+        echo "    ${CLRD}Tag ${CLBL}${TAG}${CLRD} already exists${RSTC}"
+        echo ""
+        # TAGS=$(git tag | sed -e 's/\n/ /g' | tr '\r\n' ' ')
+        # echo "    ${CGY}Existing tags: ${TAGS}${RSTC}"
+        PACKAGE_VERSION=$TAG
+
         return 1
     fi
 
@@ -37,21 +65,35 @@ function pushNewTag() {
     local TAG=$1
     splitVersion $TAG
 
-    echo ""
-    echo "    ${CGY}package.json Version: ${TAG}${RSTC}"
-    echo "    ${CGY}Tag Version:          ${TAG}${RSTC}"
-    echo ""
+    if [ "$TAG" != "" ]; then
+        echo ""
+        echo "    ${CLBL}Previous tag:          ${PACKAGE_VERSION}${RSTC}"
+        echo "    ${CLBL}Previous ${FILENAME}: ${PACKAGE_VERSION}${RSTC}"
+        echo ""
+        echo "    ${CLGN}New tag:               ${TAG}${RSTC}"
+        echo "    ${CLGN}New ${FILENAME}:      ${TAG}${RSTC}"
+        echo ""
+    else
+        TAG=$PACKAGE_VERSION
+        echo ""
+        echo "    ${CLGN}New tag:               ${TAG}${RSTC}"
+        echo "    ${CLGN}New ${FILENAME}:      ${TAG}${RSTC}"
+        echo ""
+    fi
 
     echo -n "Are you sure you want to bump the version? ${CGY}(y/n)${RSTC} "
     read ANSWER2
     local CONFIRM=$(echo $ANSWER2 | tr [:upper:] [:lower:])
 
     if [ "${CONFIRM}" == "" ] || [ "${CONFIRM}" == "y" ] || [ "${CONFIRM}" == "yes" ]; then
-        $(cat ${DIR}/package.json | sed -e s/\"${PACKAGE_VERSION}\"/\"${TAG}\"/g > ${DIR}/package1.json)
-        $(rm ${DIR}/package.json)
-        $(mv ${DIR}/package1.json ${DIR}/package.json)
-        git add . ; git commit -m "bump tag ${TAG}" ; git push
-        git tag ${TAG} ; git push origin --tags
+        $(cat ${DIR}/${FILENAME} | sed -e s/\"${PACKAGE_VERSION}\"/\"${TAG}\"/g > ${DIR}/NEW_${FILENAME})
+        $(rm ${DIR}/${FILENAME})
+        $(mv ${DIR}/NEW_${FILENAME} ${DIR}/${FILENAME})
+        # echo "${CGY}$(npm install)"
+        # echo "${CGY}$(git add . ; git commit -m "bump tag to ${TAG}" ; git push)"
+        # echo "${CGY}$(git tag ${TAG} ; git push origin --tags)${RSTC}"
+        echo "${BGCGN}${CWHT}Your tag has been bumped to${BGRSTC} ${BGCBL}${TAG}${RSTC}${BGRSTC}"
+        echo ""
     else
         echo "    ${CLRD}Process aborted!"
         echo ""
@@ -62,25 +104,25 @@ function pushNewTag() {
 function init() {
     splitVersion $PACKAGE_VERSION
 
-    echo ""
-    echo -n "Current package.json version: ${CGY}(${PACKAGE_VERSION})${RSTC} "
+    echo -n "Current ${FILENAME} version: ${CGY}(${PACKAGE_VERSION})${RSTC} "
     read VERSION
 
     if [ "$VERSION" == "" ]; then
         checkTagExists $PACKAGE_VERSION
         local TAG_1=$?
 
-        if [ $TAG_1 ]; then
+        if [ $TAG_1 -eq 1 ]; then
             echo -n "Would you like to bump the version? ${CGY}(y/n)${RSTC} "
             read ANSWER1
             local BUMP=$(echo $ANSWER1 | tr [:upper:] [:lower:])
 
             if [ "${BUMP}" == "" ] || [ "${BUMP}" == "y" ] || [ "${BUMP}" == "yes" ]; then
-                echo -n "Please enter the new version: ${CGY}(eg. 1.0.1)${RSTC} "
-                read NEW_VERSION
-
-                checkTagExists $NEW_VERSION
-                local TAG_2=$?
+                while [ "$NEW_VERSION" == "" ] || [ $TAG_2 -eq 1 ]; do
+                    echo -n "Please enter the new version different from ${CLBL}${PACKAGE_VERSION}${RSTC}: "
+                    read NEW_VERSION
+                    checkTagExists $NEW_VERSION
+                    local TAG_2=$?
+                done
 
                 if [ $TAG_2 -eq 0 ]; then
                     pushNewTag $NEW_VERSION
@@ -91,13 +133,18 @@ function init() {
                 exit
             fi
         else
-            echo All good
+            checkTagExists $VERSION
+            local TAG_3=$?
+
+            if [ $TAG_3 -eq 0 ]; then
+                pushNewTag $VERSION
+            fi
         fi
     else
         checkTagExists $VERSION
-        local TAG_3=$?
+        local TAG_4=$?
 
-        if [ $TAG_3 -eq 0 ]; then
+        if [ $TAG_4 -eq 0 ]; then
             pushNewTag $VERSION
         fi
     fi
