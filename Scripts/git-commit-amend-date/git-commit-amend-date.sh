027f53707427045abd611f6bca231823c7e5f69f
Roger Takeshita <roger.takeshita@gmail.com>
2021-02-19 00:31:16 -0500

    feat: Should get or set day - git-commit-amend-date.sh
        If no day is provided, the script will get the current date/time

--- Scripts/git-commit-amend-date/git-commit-amend-date.sh
+++ Scripts/git-commit-amend-date/git-commit-amend-date.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-YEAR=$(date +"%Y")
-MONTH=$(date +"%b")
-DAY_NUM=$(date +"%u")
-DAY_STR=$(date +"%a")
-TIME=$(date +"%T")
-
 CLBL=$'\e[38;5;117m' # light blue
 CLGN=$'\e[38;5;2m'   # light green
+
+PAST_DAY=$@
+
+if [ "$PAST_DAY" != "" ]; then
+    CURRENT_YEAR=$(DATE +"%Y")
+    CURRENT_MONTH=$(date +"%m")
+    DATE_EPOCH=$(date -jf "%Y-%m-%d %H:%M:%S" "${CURRENT_YEAR}-${CURRENT_MONTH}-${PAST_DAY} 19:45:00" +%s)
+    DATE="date -r ${DATE_EPOCH}"
+else
+    DATE="date"
+fi
+
+YEAR=$(${DATE} +"%Y")
+MONTH=$(${DATE} +"%b")
+DAY_NUM=$(${DATE} +"%-d")
+DAY_STR=$(${DATE} +"%a")
+TIME=$(date +"%T")
 
 command="git commit --amend --no-edit --date=\"${DAY_STR} ${MONTH} ${DAY_NUM} ${TIME} ${YEAR} -0500\""
 
