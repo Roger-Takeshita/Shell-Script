2748809b410d80a76ada6fd4c452fa7c5c97d7d2
Roger Takeshita <roger.takeshita@gmail.com>
2021-02-24 20:06:31 -0500

    feat: Should add error handling postgres-dump.sh

--- Scripts/postgres-dump/postgres-dump.sh
+++ Scripts/postgres-dump/postgres-dump.sh
@@ -19,6 +19,25 @@ MONTH=$(date +"%m")
 DAY=$(date +"%d")
 DATABASE_FILE=$@
 
+function simpleErrorMsg () {
+    local STATUS=$1
+
+    if [[ "$STATUS" =~ .*"skipping".* ]]; then
+    elif [ "${STATUS}" != "" ]; then
+        echo "   ${CLRD}${STATUS}${RSTC}"
+        echo ""
+        exit 1
+    fi
+}
+
+function deleteFolder () {
+    local FOLDER=$1
+
+    if [ -d "$FOLDER" ]; then
+        rm -rf $FOLDER
+    fi
+}
+
 if [ ${#DATABASE_FILE} -eq 0 ]; then
     echo ""
     echo "   ${CLRD}Process aborted: ${CLOG}please provide a database name.${RSTC}"
@@ -46,21 +65,18 @@ fi
 POSTGRES_KEY=$(cat "${POSTGRES_KEY_PATH}")
 DUMP_FOLDER="${DESKTOP_DIR}/${YEAR}-${MONTH}-${DAY}_dump"
 DATABASE_NAME="prepanywhere_dev_${DAY}_${MONTH}_${YEAR}"
-
-if [ -d "$DUMP_FOLDER" ]; then
-    rm -rf $DUMP_FOLDER
-fi
+deleteFolder $DUMP_FOLDER
 
 echo ""
-echo "   ${CLGN}Dumping the database into ${CBL}${DUMP_FOLDER}${CLGN}...${RSTC}"
+echo "   ${CLGN}Dumping remote database into ${CBL}${DUMP_FOLDER}${RSTC}"
 pg_dump -j 8 -Fd $POSTGRES_KEY -f $DUMP_FOLDER
-echo "   ${CLGN}Dropping the database ${CBL}${DATABASE_NAME}${CLGN}...${RSTC}"
-dropdb $DATABASE_NAME
-echo "   ${CLGN}Creating the database ${CBL}${DATABASE_NAME}${CLGN}...${RSTC}"
-createdb $DATABASE_NAME
-echo "   ${CLGN}Restoring the database ${CBL}${DATABASE_NAME}${CLGN}...${RSTC}"
+zip -r "$DUMP_FOLDER/${YEAR}-${MONTH}-${DAY}_dump" "${DUMP_FOLDER}" > /dev/null 2>&1
+STATUS1=$(dropdb $DATABASE_NAME --if-exists 2>&1 1>/dev/null)
+simpleErrorMsg $STATUS1
+STATUS2=$(createdb $DATABASE_NAME 2>&1 1>/dev/null)
+simpleErrorMsg $STATUS2
+echo "   ${CLGN}Restoring local database ${CBL}${DATABASE_NAME}${RSTC}"
 pg_restore -j 8 -d $DATABASE_NAME $DUMP_FOLDER
-zip -r $DUMP_FOLDER $DUMP_FOLDER
 echo ""
 echo "${BGCGN}${CWHT}All Done!${RSTC}${BGRSTC}"
 echo ""
