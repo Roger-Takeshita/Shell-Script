8a62ae0d5e91c57da27e7c5a4afb642dc4a6a305
Roger Takeshita <roger.takeshita@gmail.com>
2021-03-04 18:29:34 -0500

    feat: Should add support to dump multiple database (postgres-dump.sh)

--- Scripts/postgres-dump/postgres-dump.sh
+++ Scripts/postgres-dump/postgres-dump.sh
@@ -11,13 +11,16 @@ CLOG=$'\e[38;5;215m'  # light orange
 CLRD=$'\e[38;5;1m'    # light red
 BGRSTC=$'\e[49m'      # bg reset color
 BGCGN=$'\e[48;5;34m'  # bg green
+BGCOG=$'\e[48;5;166m' # bg orange
 
 HOME_DIR=$(eval echo "~$USER")
 DESKTOP_DIR="${HOME_DIR}/Desktop"
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 DAY=$(date +"%d")
-DATABASE_FILE=$@
+DATABASE_NAME=$@
+SCRIPT_DIR=$(dirname "$0")
+DATABASE_FILE="${SCRIPT_DIR}/database_connection.json"
 
 function simpleErrorMsg () {
     local STATUS=$1
@@ -38,7 +41,7 @@ function deleteFolder () {
     fi
 }
 
-if [ ${#DATABASE_FILE} -eq 0 ]; then
+if [ ${#DATABASE_NAME} -eq 0 ]; then
     echo ""
     echo "   ${CLRD}Process aborted: ${CLOG}please provide a database name.${RSTC}"
     echo ""
@@ -52,31 +55,37 @@ if [ $# -gt 1 ]; then
     exit 1
 fi
 
-POSTGRES_KEY_PATH="/Users/roger-that/Documents/Roger-That/Dev/1-Config/PostgreSQL/${DATABASE_FILE}.txt"
-
-if [ ! -f "$POSTGRES_KEY_PATH" ]; then
+if [ ! -f "$DATABASE_FILE" ]; then
     echo ""
     echo "   ${CLRD}Process aborted: ${CLOG}file doesn't exist.${RSTC}"
-    echo "   ${CBL}${POSTGRES_KEY_PATH}${RSTC}"
+    echo "   ${CBL}${DATABASE_FILE}${RSTC}"
     echo ""
     exit 1
 fi
 
-POSTGRES_KEY=$(cat "${POSTGRES_KEY_PATH}")
-DUMP_FOLDER="${DESKTOP_DIR}/${YEAR}-${MONTH}-${DAY}_dump"
-DATABASE_NAME="prepanywhere_dev_${DAY}_${MONTH}_${YEAR}"
+POSTGRES_URL=$(cat $DATABASE_FILE | grep "\"${DATABASE_NAME}\":" | sed 's/[\",]//g' | sed -e "s/\(${DATABASE_NAME}:\) \(.*\)/\2/" | xargs)
+
+if [ "$POSTGRES_URL" = "" ]; then
+    echo ""
+    echo "   ${CLRD}Process aborted: ${BGCOG}${CWHT}${DATABASE_NAME}${BGRSTC}${CLOG} connection URL not found. Please double check if you have a field named ${BGCOG}${CWHT}${DATABASE_NAME}${BGRSTC} ${CLOG}in ${CBL}${DATABASE_FILE}${RSTC}"
+    echo ""
+    exit 1
+fi
+
+DUMP_FOLDER="${DESKTOP_DIR}/${YEAR}-${MONTH}-${DAY}_${DATABASE_NAME}_dump"
+LOCAL_DATABASE_NAME="${DATABASE_NAME}_dev_${YEAR}_${MONTH}_${DAY}"
 deleteFolder $DUMP_FOLDER
 
 echo ""
 echo "   ${CLGN}Dumping remote database into ${CBL}${DUMP_FOLDER}${RSTC}"
-pg_dump -j 8 -Fd $POSTGRES_KEY -f $DUMP_FOLDER
-zip -r "$DUMP_FOLDER/${YEAR}-${MONTH}-${DAY}_dump" "${DUMP_FOLDER}" > /dev/null 2>&1
-STATUS1=$(dropdb $DATABASE_NAME --if-exists 2>&1 1>/dev/null)
+pg_dump -j 8 -Fd $POSTGRES_URL -f $DUMP_FOLDER
+zip -r "$DUMP_FOLDER/${YEAR}-${MONTH}-${DAY}_${DATABASE_NAME}_dump" "${DUMP_FOLDER}" > /dev/null 2>&1
+STATUS1=$(dropdb $LOCAL_DATABASE_NAME --if-exists 2>&1 1>/dev/null)
 simpleErrorMsg $STATUS1
-STATUS2=$(createdb $DATABASE_NAME 2>&1 1>/dev/null)
+STATUS2=$(createdb $LOCAL_DATABASE_NAME 2>&1 1>/dev/null)
 simpleErrorMsg $STATUS2
-echo "   ${CLGN}Restoring local database ${CBL}${DATABASE_NAME}${RSTC}"
-pg_restore -j 8 -d $DATABASE_NAME $DUMP_FOLDER
+echo "   ${CLGN}Restoring local database ${CBL}${LOCAL_DATABASE_NAME}${RSTC}"
+pg_restore -j 8 -d $LOCAL_DATABASE_NAME $DUMP_FOLDER
 echo ""
 echo "${BGCGN}${CWHT}All Done!${RSTC}${BGRSTC}"
 echo ""
