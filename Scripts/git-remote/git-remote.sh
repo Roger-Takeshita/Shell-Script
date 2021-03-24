12f474d7735b4a5208b663bb5a3cf8eb1db8e505
Roger Takeshita <roger.takeshita@gmail.com>
2021-03-24 19:09:16 -0400

    feat: Should open github repository using chrome (git-remote.sh)

--- Scripts/git-remote/git-remote.sh
+++ Scripts/git-remote/git-remote.sh
@@ -5,9 +5,20 @@
 
 RSTC=$'\e[39m'        # reset color
 CLBL=$'\e[38;5;117m'  # light blue
+OPEN_FILE=1
+
+if [[ "$@" =~ .*"-n".* ]]; then
+    OPEN_FILE=0
+fi
 
 REMOTE_URL=$(git remote -v | grep fetch | sed -e 's/origin//g' | sed -e 's/[()]//g' | sed -e 's/fetch//g' | sed -e 's/\.git//g'  | xargs)
 
 echo ""
 echo "   ${CLBL}${REMOTE_URL}${RSTC}"
 echo ""
+
+if [ $OPEN_FILE -eq 1 ]; then
+    open $REMOTE_URL
+fi
+
+echo "${REMOTE_URL}" | tr -d '\n'| pbcopy
