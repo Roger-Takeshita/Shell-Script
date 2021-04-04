40c50eab128a4250d51620a0f5e4db3662efb360
Roger Takeshita <roger.takeshita@gmail.com>
2021-04-04 13:51:30 -0400

    feat: Should update tmux dev initial script

--- Scripts/tmux/dev-terminal.sh
+++ Scripts/tmux/dev-terminal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # tmux clear-history
-# tmux new-session -s Dev -n DevOne
-# tmux select-window -t Dev:DevOne
+# tmux new-session -s Dev -n One
+# tmux select-window -t Dev:One
 tmux rename-session Dev
-tmux rename-window DevOne
+tmux rename-window One
 tmux split-window -v
 tmux split-window -h
