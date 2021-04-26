8fd0b42b35a85ef562fb2770e49037db906050a4
Roger Takeshita <roger.takeshita@gmail.com>
2021-04-26 18:51:07 -0400

    feat: Should double check before dropping stash (git-stash-drop.sh)

--- Scripts/git-stash-drop/git-stash-drop.sh
+++ Scripts/git-stash-drop/git-stash-drop.sh
@@ -3,10 +3,25 @@
 # Developed by Roger Takeshita
 # https://github.com/Roger-Takeshita/Shell-Script
 
+RSTC=$'\e[39m'        # reset color
+CLGY=$'\e[38;5;240m'  # gray
+CLOG=$'\e[38;5;215m'  # light orange
+RSTF=$'\e[0m'
+Dim=$'\e[2m'
+Bold=$'\e[1m'
+
 NUM=$1
 
 if [ ! $NUM ]; then
-    git stash drop stash@{0}
-else
+    NUM=0
+fi
+
+echo ""
+echo -n "Are you sure you want to drop stash ${CLOG}${Bold}${NUM}${RSTC}${RSTF}: ${Dim}${CLGY}(Y/n)${RSTC}${RSTF} "
+read ANSWER
+
+if [ "$ANSWER" = "" ] || [ "$ANSWER" = "Y" ] || [ "$ANSWER" = "y" ] || [ "$ANSWER" = "Yes" ] || [ "$ANSWER" = "yes" ]; then
     git stash drop stash@{$NUM}
 fi
+
+echo ""
