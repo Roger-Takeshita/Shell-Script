0d93fa2abeba5cc34c00e383255be1ab94900bf8
Roger Takeshita <roger.takeshita@gmail.com>
2021-05-29 11:58:13 -0400

    feat: Should add normal touch command and dry the code (touch-open.sh)

--- Scripts/touch-open/touch-open.sh
+++ Scripts/touch-open/touch-open.sh
@@ -11,100 +11,90 @@ DIR=''
 PREV_DIR=''
 OPEN_FILE=0
 JOIN_FLAG=0
+NORMAL_TOUCH=0
 
-if [ $# -eq 0 ]; then
-    echo "${CLRD}ERROR:${RSTC} I need a file"
-    exit 22
-fi
+[ $# -eq 0 ] && echo "${CLRD}ERROR:${RSTC} I need a file" && exit 22
 
 touchFile () {
     local DIRECTORY=$1
     local FILE=$2
     local FILE_PATH="${DIRECTORY}${FILE}"
 
-    if [ ! -f $FILE_PATH ]; then
-        touch $FILE_PATH
-    else
-        printf "%s\n" "${DIRECTORY}${CLGN}${FILE}${RSTC} - ${CLRD}Already Exists${RSTC}"
-    fi
-
-    if [ $OPEN_FILE -eq 1 ]; then
-        code $FILE_PATH
-    fi
+    [ -f $FILE_PATH ] && printf "%s\n" "${DIRECTORY}${CLGN}${FILE}${RSTC} - ${CLRD}Already Exists${RSTC}"
+    [ ! -f $FILE_PATH ] && touch $FILE_PATH
+    [ $OPEN_FILE -eq 1 ] && [ ! -z $EDITOR ] && $EDITOR $FILE_PATH
 }
 
 makeDir () {
     local FOLDER=$1
 
-    if [ ! -d "$FOLDER" ]; then
-        mkdir -p "$FOLDER"
-    fi
+    [ ! -d $FOLDER ] && mkdir -p $FOLDER
 }
 
 touchFiles () {
     local FILES=$@
-    if [[ "$FILES" =~ (.*[ ]?-y[ $]?) ]]; then
-        OPEN_FILE=1
-    fi
 
-    for ITEM1 in $FILES; do
-        IFS='/' read -ra FOLDER <<< "$ITEM1"
-        LEN=${#FOLDER[@]}
+    [[ "$FILES" =~ (.*[ ]?-y[ $]?) ]] && OPEN_FILE=1
+    [[ "$FILES" =~ (.*[ ]?-n[ $]?) ]] && NORMAL_TOUCH=1
 
-        if [ $JOIN_FLAG -eq 0 ]; then
-            DIR=''
-        fi
+    if [ $NORMAL_TOUCH -eq 0 ]; then
+        for ITEM1 in $FILES; do
+            IFS='/' read -ra FOLDER <<< "$ITEM1"
+            LEN=${#FOLDER[@]}
 
-        if [ $LEN -gt 1 ]; then
-            for ITEM2 in ${FOLDER[@]}; do
-                if [ "$ITEM2" == "Dockerfile" ] || [ "$ITEM2" == "Procfile" ] || [ "$ITEM2" == "LICENSE" ]; then
-                    touchFile $DIR $ITEM2
-                    continue
-                elif [ "$ITEM2" == ".ssh" ] || [ "$ITEM2" == ".ssl" ] || [ "$ITEM2" == ".vscode" ]; then
-                    DIR="${DIR}${ITEM2}/"
-                    makeDir $DIR
-                    continue
-                fi
+            [ $JOIN_FLAG -eq 0 ] && DIR=''
 
-                if [[ "$ITEM2" =~ .*\..* ]]; then
-                    touchFile $DIR $ITEM2
-                else
-                    DIR="${DIR}${ITEM2}/"
-
-                    if [ "$ITEM2" != "${FOLDER[$LEN-1]}" ]; then
-                        PREV_DIR=$DIR
+            if [ $LEN -gt 1 ]; then
+                for ITEM2 in ${FOLDER[@]}; do
+                    if [ "$ITEM2" == "Dockerfile" ] || [ "$ITEM2" == "Procfile" ] || [ "$ITEM2" == "LICENSE" ]; then
+                        touchFile $DIR $ITEM2
+                        continue
+                    elif [ "$ITEM2" == ".ssh" ] || [ "$ITEM2" == ".ssl" ] || [ "$ITEM2" == ".vscode" ]; then
+                        DIR="${DIR}${ITEM2}/"
+                        makeDir $DIR
+                        continue
                     fi
 
-                    makeDir $DIR
-                fi
-            done
-        else
-            if [ $ITEM1 == "+" ]; then
-                DIR="${PREV_DIR}/"
-                JOIN_FLAG=1
-                continue
-            elif [ $ITEM1 == "-y" ]; then
-                continue
-            fi
-
-            if [[ "$ITEM1" =~ .*\..* ]]; then
-                if [ $JOIN_FLAG -eq 1 ]; then
-                    JOIN_FLAG=0
-                else
-                    DIR=''
-                fi
-
-                touchFile $DIR $ITEM1
+                    if [[ "$ITEM2" =~ .*\..* ]]; then
+                        touchFile $DIR $ITEM2
+                    else
+                        DIR="${DIR}${ITEM2}/"
+                        [ "$ITEM2" != "${FOLDER[$LEN-1]}" ] && PREV_DIR=$DIR
+                        makeDir $DIR
+                    fi
+                done
             else
-                if [ $JOIN_FLAG -eq 1 ]; then
-                    JOIN_FLAG=0
-                    makeDir "${DIR}${ITEM1}/"
+                if [ $ITEM1 == "+" ]; then
+                    DIR="${PREV_DIR}/"
+                    JOIN_FLAG=1
+                    continue
+                elif [ $ITEM1 == "-y" ]; then
+                    continue
+                fi
+
+                if [[ "$ITEM1" =~ .*\..* ]]; then
+                    if [ $JOIN_FLAG -eq 1 ]; then
+                        JOIN_FLAG=0
+                    else
+                        DIR=''
+                    fi
+
+                    touchFile $DIR $ITEM1
                 else
-                    makeDir $ITEM1
+                    if [ $JOIN_FLAG -eq 1 ]; then
+                        JOIN_FLAG=0
+                        makeDir "${DIR}${ITEM1}/"
+                    else
+                        makeDir $ITEM1
+                    fi
                 fi
             fi
-        fi
-    done
+        done
+    else
+        UPDATE_FILES=$(echo "$FILES" | sed -E 's/(-n|-y)[ $]?//g' | xargs)
+        touch $UPDATE_FILES
+        [ $OPEN_FILE -eq 1 ] && [ ! -z $EDITOR ] && $EDITOR $UPDATE_FILES
+    fi
 }
 
 touchFiles $@
