286312a0eab668110f3a6e55eb0ea92f2aab9aeb
Roger Takeshita <roger.takeshita@gmail.com>
2021-05-30 01:30:20 -0400

    feat: Should open repository branch (git-remote.sh)

--- Scripts/git-remote/git-remote.sh
+++ Scripts/git-remote/git-remote.sh
@@ -6,22 +6,33 @@
 RSTC=$'\e[39m'        # reset color
 CLBL=$'\e[38;5;117m'  # light blue
 OPEN_FILE=1
+MAIN=0
 
-if [[ "$@" =~ .*"-n".* ]]; then
-    OPEN_FILE=0
-fi
+[[ "$@" =~ (.*[ ]?-n[ $]?) ]] && OPEN_FILE=0
+[[ "$@" =~ (.*[ ]?-m[ $]?) ]] && MAIN=1
 
-REMOTE_URL=$(git remote -v | grep fetch | sed -e 's/origin//g' -e 's/[()]//g' -e 's/fetch//g' -e 's/\.git//g' -e 's/git@github.com://g' | xargs )
+copyURL() {
+    local URL=$1
+    echo ""
+    echo "   ${CLBL}${URL}${RSTC}"
+    echo ""
+    echo "${URL}" | tr -d '\n'| pbcopy
+}
 
-if [[ "$REMOTE_URL" != "https"* ]]; then
-  REMOTE_URL="https://github.com/${REMOTE_URL}"
-fi
+gitRemote() {
+    local CMD=$@
+    BRANCH_NAME=$(git branch --show-current)
+    REMOTE_URL=$(git remote -v | grep fetch | sed -e 's/origin//g' -e 's/[()]//g' -e 's/fetch//g' -e 's/\.git//g' -e 's/git@github.com://g' -e 's/https:\/\/github.com\///' | xargs )
 
-if [ $OPEN_FILE -eq 1 ]; then
-    open $REMOTE_URL
-else
-  echo ""
-  echo "   ${CLBL}${REMOTE_URL}${RSTC}"
-  echo ""
-  echo "${REMOTE_URL}" | tr -d '\n'| pbcopy
-fi
+    # if $(git ls-remote --heads git@github.com:${REMOTE_URL}.git ${BRANCH_NAME}); then
+    [ $MAIN -eq 0 ] && REMOTE_URL="https://github.com/${REMOTE_URL}/tree/${BRANCH_NAME}"
+    [ $MAIN -eq 1 ] && REMOTE_URL="https://github.com/${REMOTE_URL}"
+
+    if [ $OPEN_FILE -eq 1 ]; then
+        open ${REMOTE_URL}
+    else
+        copyURL $REMOTE_URL
+    fi
+}
+
+gitRemote $@
