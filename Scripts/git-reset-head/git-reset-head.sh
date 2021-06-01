31a28069af37be4d5fd7c0cfe21c1343baac0dd2
Roger Takeshita <roger.takeshita@gmail.com>
2021-06-01 01:09:04 -0400

    feat: Should git reset multiple items (git-reset-head.sh)

--- Scripts/git-reset-head/git-reset-head.sh
+++ Scripts/git-reset-head/git-reset-head.sh
@@ -8,19 +8,17 @@ ARGS=$@
 HARD=0
 NUM=0
 
-if [[ "$@" =~ -+hard($| )|-+HARD($| )|-+h($| ) ]]; then
-    HARD=1;
-fi
-
-if [[ "$@" =~ .*([0-9]+).* ]]; then
-    NUM=${BASH_REMATCH[1]}
-fi
+[[ "$@" =~ -+hard($| )|-+HARD($| )|-+h($| ) ]] && HARD=1
+[[ "$@" =~ .*([0-9]+).* ]] && NUM=${BASH_REMATCH[1]}
 
 if [ $NUM -eq 0 ] && [ $HARD -eq 0 ]; then
-    if [ $ARGS ]; then
-        git reset $ARGS
+    if [ $# -gt 0 ]; then
+        ARGS_ARRARY=($(echo "$ARGS" | sed -E 's/[ ]*(\?|M|D|N new file:)[ ]*/ /g' | xargs))
+        for ITEM in ${ARGS_ARRARY[*]}; do
+            git reset $ITEM 1>/dev/null
+        done
     else
-        git reset
+        git reset 1>/dev/null
     fi
 elif [ $NUM -eq 0 ] && [ $HARD -eq 1 ]; then
     git reset HEAD~1 --hard
